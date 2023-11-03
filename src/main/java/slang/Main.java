93edd3a40f456f48979204043b96fa7ecab680d3
tmtrung21@clc.fitus.edu.vn <simongin@Simons-MacBook-Pro.local>
2023-11-03 15:22:30 +0700

    Change Type of SlangDictionary to HashMap for a better use

--- src/main/java/slang/Main.java
+++ src/main/java/slang/Main.java
@@ -4,20 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
 //        SlangWindow window = new SlangWindow();
-//        BufferedReader buffReader = new BufferedReader(new FileReader("slang.txt"));
-//        String line = buffReader.readLine();
-//        System.out.println(line);
+
         SlangDictionary dictionary = new SlangDictionary();
-        SlangWord word = new SlangWord("&","And");
-        SlangWord word2 = new SlangWord("&","An");
-        System.out.println(dictionary.addSlang(word));
-        System.out.println(dictionary.addSlang(word2));
+        dictionary.importFile();
 
 
 
--- src/main/java/slang/SlangDictionary.java
+++ src/main/java/slang/SlangDictionary.java
@@ -1,16 +1,38 @@
 package slang;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class SlangDictionary {
-    private Set<SlangWord> dictionary;
+    private HashMap<String,SlangWord> dictionary;
 
     public SlangDictionary() {
-        this.dictionary = new HashSet<>();
+        this.dictionary = new HashMap<>();
     }
 
     public boolean addSlang(SlangWord word) {
-        return dictionary.add(word);
+        if (dictionary.containsKey(word.getKey())) {
+            return false;
+        }
+        dictionary.put(word.getKey(), word);
+        return true;
     }
+
+    public void importFile() throws IOException {
+        BufferedReader buffReader = new BufferedReader(new FileReader("slang.txt"));
+        String line;
+        while ((line = buffReader.readLine()) != null) {
+            String[] parts = line.split("`");
+            SlangWord newWord = new SlangWord(parts[0],parts[1]);
+            addSlang(newWord);
+        }
+        buffReader.close();
+    }
+
+//    public boolean searchKey(String key) {
+//        SlangWord needleSlang = new SlangWord(key," ");
+//        return dictionary.contains(needleSlang);
+//    }
 }
